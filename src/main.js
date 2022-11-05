1d77590c3618c159c530e079c5be9482dcbda41c
Nina Vergmann <nina152b@kea.stud.dk>
2022-11-05 15:28:17 +0100

    SIDSTE ÆNDRINGER SLUT

--- src/main.js
+++ src/main.js
@@ -2,9 +2,4 @@ import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
-document.querySelectorAll(".parallaxcontainer").forEach((parallaxcontainer) => {
-  const elementderskalparallaxes = parallaxcontainer.querySelector("p");
-  scroll(animate(elementderskalparallaxes, { y: [50, 500] }), {
-    target: elementderskalparallaxes,
-  });
-});
+scroll(animate(".progress-bar", { scaleX: [0, 1] }));
